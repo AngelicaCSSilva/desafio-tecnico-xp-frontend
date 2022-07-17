0f16dd1a24b4bef56a2619acf83db3f82c7785c5
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-17 02:26:12 -0300

    feat: :sparkles: Add routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Bank from './components/pages/Bank';
+import Login from './components/pages/Login';
+import Stocks from './components/pages/Stocks';
 
 function App() {
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/stocks" element={<Stocks />} />
+        <Route path="/bank" element={<Bank />} />
+      </Routes>
     </div>
   );
 }
