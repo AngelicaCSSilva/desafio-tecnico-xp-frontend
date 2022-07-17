1b7b451384ea118694974ea2a48b1596d2e20ae7
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-17 04:17:11 -0300

    feat: :sparkles: Store user email in Context

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import FinanceContext from '../../context/FinanceContext';
 import { getLocalStorage, saveLocalStorage } from '../../utils/localStorage';
 import { CenteredForm } from './styles/CenteredForm';
 import { StyledLabel } from './styles/StyledLabel';
 
 export default function LoginForm() {
+  const { setUserEmail } = useContext(FinanceContext);
   const navigate = useNavigate();
 
   const [user, setUser] = useState({
@@ -21,6 +23,7 @@ export default function LoginForm() {
 
   const handleClickButton = () => {
     saveLocalStorage('lastUser', { email: user.email, lastLogin: new Date() });
+    setUserEmail(user.email);
     navigate('/stocks', { replace: true });
   };
 
