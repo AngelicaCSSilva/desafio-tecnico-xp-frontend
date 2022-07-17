b91ac8862e329171531ada18f8fc34e2156ebd22
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-17 04:17:36 -0300

    feat: :sparkles: Add login form to login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 import Logo from '../img/Logo';
 
 export default function Login() {
   return (
     <>
       <Logo width="120" />
+      <LoginForm />
     </>
   );
 }
