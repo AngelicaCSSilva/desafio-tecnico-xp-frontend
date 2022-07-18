91acfa15b209c697cd7ef4d03e2731fb482a8804
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-18 16:35:54 -0300

    feat: :sparkles: Create stock page

--- src/components/pages/Stocks.jsx
+++ src/components/pages/Stocks.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import FinanceContext from '../../context/FinanceContext';
+import Footer from '../footer/Footer';
+import Loader from '../loader/Loader';
+import StocksTable from '../stocks/StocksTable';
 
 export default function Stocks() {
+  const {
+    investments,
+    generateGlobalState,
+    userName,
+  } = useContext(FinanceContext);
+
+  useEffect(() => {
+    generateGlobalState();
+  }, []);
+
+  const userStocks = investments?.stocks?.map((stock) => stock.ticket);
+  const userInvestments = investments?.stocks;
+
+  const watchlistExclusives = investments?.watchlist.filter((stock) => !userStocks.includes(stock));
+
   return (
-    <h1>Stock</h1>
+    <div>
+      { !userStocks
+        ? <Loader />
+        : (
+          <>
+            <div><p>{`Olá, ${userName}`}</p></div>
+            <h2>Minhas ações</h2>
+            <StocksTable stocks={userStocks} userInvestments={userInvestments} />
+            <h2>Disponíveis para investir</h2>
+            <StocksTable stocks={watchlistExclusives} isOnlyBuyOp />
+            <Footer />
+          </>
+        )}
+    </div>
   );
 }
