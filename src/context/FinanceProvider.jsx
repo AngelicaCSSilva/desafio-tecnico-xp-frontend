b1ce7e0eb5c22aaaa8e16ed868928db24b6c9036
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-18 16:33:36 -0300

    style: :art: Order imports

--- src/context/FinanceProvider.jsx
+++ src/context/FinanceProvider.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
 import FinanceContext from './FinanceContext';
 
 function FinanceProvider({ children }) {
