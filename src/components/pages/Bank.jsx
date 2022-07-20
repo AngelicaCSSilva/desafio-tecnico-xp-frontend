41d426228a08d08cc3e1f7f02c4a3b2154235693
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-20 18:36:48 -0300

    feat: :sparkles: Create bank page

--- src/components/pages/Bank.jsx
+++ src/components/pages/Bank.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
+import FinanceContext from '../../context/FinanceContext';
+import Balance from '../bank/Balance';
+import StatementTable from '../bank/StatementTable';
+import OperationButton from '../button/OperationButton';
+import { StyledButtonsDiv } from '../button/StyledButtonsDiv';
+import Footer from '../footer/Footer';
+import Loader from '../loader/Loader';
+import Modal from '../modal/Modal';
 
 export default function Bank() {
+  const { bankTransactions, isModalOn } = useContext(FinanceContext);
+  const [transactions, setTransactions] = useState(null);
+  const [isGettingData, setIsGettingData] = useState(true);
+
+  useEffect(() => {
+    setIsGettingData(true);
+    setTransactions(bankTransactions?.transaction);
+    setIsGettingData(false);
+  }, [bankTransactions]);
+
   return (
-    <h1>Bank</h1>
+    <>
+      { isGettingData
+        ? <Loader />
+        : (
+          <main>
+            <h1>Bank</h1>
+            <Balance transactions={transactions} />
+            <StyledButtonsDiv>
+              <OperationButton operationType="Retirada" />
+              <OperationButton operationType="Depósito" />
+            </StyledButtonsDiv>
+            <h2>Extrato bancário</h2>
+            <StatementTable transactions={transactions} />
+            {isModalOn && <Modal />}
+          </main>
+        )}
+      <Footer />
+    </>
   );
 }
