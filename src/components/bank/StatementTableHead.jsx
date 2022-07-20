c26196f29c9fde618aabd4b03ac1bbbeb9a6f805
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-20 18:34:30 -0300

    feat: :construction: Create statement table head

---
+++ src/components/bank/StatementTableHead.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { TableHead } from './styles/TableHead.style';
+
+export default function StatementTableHead() {
+  return (
+    <thead>
+      <tr>
+        <TableHead>Data</TableHead>
+        <TableHead>Descrição</TableHead>
+        <TableHead>Valor (R$)</TableHead>
+      </tr>
+    </thead>
+  );
+}
