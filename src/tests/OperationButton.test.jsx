6720a1f9a261c05ccc5aa016a48215162b751e37
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-20 02:37:43 -0300

    test: :test_tube: Create OperationButton test

---
+++ src/tests/OperationButton.test.jsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+
+import OperationButton from '../components/button/OperationButton';
+
+describe('Operation Button test.', () => {
+  it('If receives "Compra" as operationType, must have "Compra" text.', async () => {
+    render(<OperationButton operationType="Compra" />);
+    const emailInput = screen.getByRole('button', { name: /Compra/i });
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  it('If receives "Venda" as operationType, must have "Venda" text.', async () => {
+    render(<OperationButton operationType="Venda" />);
+    const emailInput = screen.getByRole('button', { name: /Venda/i });
+    expect(emailInput).toBeInTheDocument();
+  });
+});
