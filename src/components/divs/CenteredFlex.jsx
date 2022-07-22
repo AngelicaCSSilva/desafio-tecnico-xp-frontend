7423f618cd06d610a0c5e36ff2bd4cdacedcfe6f
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-22 20:21:59 -0300

    feat: :art: Create centered div flex

---
+++ src/components/divs/CenteredFlex.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const CenteredFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  min-width: 100vw;
+  & > main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  & > main > h2 {
+    align-self:flex-start;
+  }
+`;
+
+export default { CenteredFlex };
