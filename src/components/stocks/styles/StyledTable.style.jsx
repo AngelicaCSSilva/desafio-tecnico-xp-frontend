c5e8c8fe4be751ad24b5b1cf48bf3d6613ee74b7
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-22 22:14:37 -0300

    feat: :art: Create styled table

---
+++ src/components/stocks/styles/StyledTable.style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledTable = styled.table`
+text-align: center;
+vertical-align: middle;
+margin-bottom: 2em;
+`;
+
+export default { StyledTable };
