01503057075d7642b8be083c1952b2e1989acb30
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-23 17:45:06 -0300

    style: :art: Add width and change justify content

--- src/components/button/StyledButtonsDiv.jsx
+++ src/components/button/StyledButtonsDiv.jsx
@@ -4,10 +4,10 @@ export const StyledButtonsDiv = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
   align-content: center;
-  width: 100vw;
+  width: 350px;
 `;
 
 export default { StyledButtonsDiv };
