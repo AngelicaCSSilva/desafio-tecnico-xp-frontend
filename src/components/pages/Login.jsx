b2841607911d9397dafa097130bb70b44f423795
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-22 22:17:16 -0300

    feat: :art: Format / Style - Login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import { CenteredFlex } from '../divs/CenteredFlex';
 import LoginForm from '../forms/LoginForm';
 import Logo from '../img/Logo';
 
 export default function Login() {
   return (
-    <>
+    <CenteredFlex>
       <Logo width="120" />
       <LoginForm />
-    </>
+    </CenteredFlex>
   );
 }
