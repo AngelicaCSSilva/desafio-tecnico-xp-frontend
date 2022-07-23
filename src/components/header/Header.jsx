4a4eb9601e4680b3845e453d393007827b4378cd
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-23 05:39:08 -0300

    feat: :sparkles: Add logout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import LoggedUser from './LoggedUser';
+import Logout from './Logout';
 import { StyledHeader } from './styles/StyledHeader.style';
+import { StyledUserDiv } from './styles/StyledUserDiv.style';
 import { UserImgExample } from './styles/UserImgExample.style';
 
 export default function Header() {
   return (
     <StyledHeader>
-      <UserImgExample />
-      <LoggedUser />
+      <StyledUserDiv>
+        <UserImgExample />
+        <LoggedUser />
+      </StyledUserDiv>
+      <Logout />
     </StyledHeader>
   );
 }
