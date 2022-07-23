80e532c911a1772af488d5879163b30b98ad01a8
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-22 22:14:53 -0300

    feat: :art: Create styled td for buttons

---
+++ src/components/stocks/styles/StyledButtonsTd.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledButtonsTd = styled.td`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+  align-content: center;
+  width: 12rem;
+`;
+
+export default { StyledButtonsTd };
