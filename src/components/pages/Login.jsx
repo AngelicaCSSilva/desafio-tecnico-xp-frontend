83a10cfb9d645f987702c89b742f892321833f6c
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-23 05:46:51 -0300

    feat: :sparkles: When rendered, checks if you have a token saved in session storage

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
+import FinanceContext from '../../context/FinanceContext';
+import { getLocalStorage } from '../../utils/localStorage';
+import { getSessionStorage } from '../../utils/sessionStorage';
 import { CenteredFlex } from '../divs/CenteredFlex';
 import LoginForm from '../forms/LoginForm';
 import Logo from '../img/Logo';
 
 export default function Login() {
+  const { setToken, generateGlobalState, setUserEmail } = useContext(FinanceContext);
+  const history = useHistory();
+  useEffect(() => {
+    const savedToken = getSessionStorage('token');
+    if (savedToken) {
+      setToken(savedToken);
+      setUserEmail(getLocalStorage('lastUser')?.email);
+      generateGlobalState();
+      history.push('/assets');
+    }
+  }, []);
+
   return (
     <CenteredFlex>
       <Logo width="120" />
