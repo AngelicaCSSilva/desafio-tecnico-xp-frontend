f8bfbebd07b4479b1161faa73cb0eab8bbf8e3dc
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-22 22:14:20 -0300

    feat: :art: Create heading styles

---
+++ src/components/heading/HeadingOne.style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeadingOne = styled.h1`
+font-size: 3em;
+margin-bottom: 5vh;
+font-weight: 500;
+font-family: inherit;
+color: #fff;
+text-align: center;
+`;
+
+export default { HeadingOne };
