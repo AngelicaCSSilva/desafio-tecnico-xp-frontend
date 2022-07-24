f4260e8f99f79a1b57e7cc879b95ce0da413d924
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-23 23:38:46 -0300

    style: :art: Add style for input and div (buttons)

--- src/components/forms/styles/ModalContentForm.style.jsx
+++ src/components/forms/styles/ModalContentForm.style.jsx
@@ -13,7 +13,14 @@ padding: 20px;
 border: 1px solid #ffc709;
 border-radius: 30px;
 width: 80vw;
-
+& > label > input {
+  margin: 0.5em 0;
+  text-align: center;
+  min-width: 120px;
+}
+& > div {
+  margin: 0.5em 0
+} 
 `;
 
 export default { ModalContentForm };
