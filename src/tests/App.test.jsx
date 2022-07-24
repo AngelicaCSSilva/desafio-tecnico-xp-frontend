f810de09c7a7d6cbb1e5091a1d6451ad193de7d2
Angélica Silva <angelica.silva9323@gmail.com>
2022-07-23 21:57:27 -0300

    test: Remove unused imports

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,36 +1,12 @@
 import '@testing-library/jest-dom';
 import {
-  render, screen, waitFor, act,
+  render, screen,
 } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from '../App';
-import Assets from '../components/pages/Assets';
-// import Login from '../components/pages/Login';
-import assets from './mocks/assets';
-import bank from './mocks/bank';
-import users from './mocks/users';
-import renderWithRouterAndProvider from './renderWithRouterAndProvider';
-
-const resolvedUrl = (url) => {
-  if (url === 'https://desafiobackend-angelica.herokuapp.com/users') {
-    return users;
-  }
-  if (url === 'https://desafiobackend-angelica.herokuapp.com/1/assets') {
-    return assets;
-  }
-  return bank;
-};
-
-const mockFetch = () => {
-  global.fetch = jest.fn().mockImplementation((url) => Promise.resolve({
-    status: 200,
-    ok: true,
-    json: () => Promise.resolve(resolvedUrl(url)),
-  }));
-};
 
 describe('App page test.', () => {
   it('If App renders login page.', async () => {
